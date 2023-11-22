b3eb6cae1b0b07bfc9b5040f114c1fbba16cf760
Ulrik Røsby <ulrik@rosby.no>
2023-11-22 18:04:52 +0100

    fix: make run.sh more flexible

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-poetry run python -m tdt17_project.train --dataset-path="/home/ulrikro/datasets/Cityscapes/" --use-test-set=False --epochs=20
+poetry run python -m tdt17_project.train --dataset-path="/home/ulrikro/datasets/Cityscapes/" --use-test-set=False --epochs=20 $@
